638cd2ac0142c9be3299d3b7a809763d79b0c37a
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-11 18:52:24 -0500

    Completed dragon.js

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.greet = function() {
+      return `Hi, ${rider}!`
+    };
+    this.hungry = true;
+    this.count = 0;
+    this.eat = function() {
+      this.count += 1;
+      if (this.count === 3) {
+        this.hungry = false;
+      } else {
+        this.hungry = true
+      }
+    }
+  }
+}
 
+
+
+module.exports = Dragon;
