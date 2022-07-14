8752963759eb80c5e0b97d8fd1341373ff351682
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-13 19:26:45 -0500

    Completed vampire.js

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -4,11 +4,13 @@ class Vampire {
     this.pet = pet || "bat";
     this.thirsty = true;
     this.ouncesDrank = 0;
-    this.drink = function() {
-      this.thirsty = false;
+  }
+  drink() {
+    this.thirsty = false;
+    if (this.ouncesDrank === 50) {
+      return "I'm too full to drink anymore!";
+    } else {
       this.ouncesDrank = this.ouncesDrank + 10;
-      // return "I'm too full to drink anymore!"
-      // as a result of vampire.drink === 50;
     }
   }
 }
