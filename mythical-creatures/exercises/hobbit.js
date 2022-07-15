c9d9182cd6e16ba23836dbbdd9b642b7c0f4ed4a
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-14 19:57:28 -0500

    Completed hobbit.js

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,6 +1,27 @@
 class Hobbit {
-  constructor(name) {
+  constructor({name}) {
     this.name = name;
+    this.age = 0;
+    this.adult = false;
+    this.old = false;
+    this.hasRing = false;
+  }
+  celebrateBirthday() {
+    this.age += 1;
+    if (this.age >= 101) {
+      this.adult = true;
+      this.old = true;
+    } else if (this.age >= 33) {
+      this.adult = true;
+    }
+  }
+  getRing() {
+    if (this.name === "Frodo") {
+      this.hasRing = true;
+      return "Here is the ring!"
+    } else {
+      return "You can't have it!"
+    }
   }
 }
 
