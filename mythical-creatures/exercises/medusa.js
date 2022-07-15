97b53e0a1974db686974b1afd96005017a2e9622
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-15 14:07:38 -0500

    Started medusa.js

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,13 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+}
+
+
+
+module.exports = Medusa;
--- mythical-creatures/test/medusa-test.js
+++ mythical-creatures/test/medusa-test.js
@@ -5,27 +5,31 @@ var Statue = require('../exercises/statue');
 
 describe('Medusa', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     assert.isFunction(Medusa);
   });
 
-  it.skip('should instantiate our good friend, Medusa', function() {
+  it('should instantiate our good friend, Medusa', function() {
     var medusa = new Medusa();
 
     assert.instanceOf(medusa, Medusa);
   });
 
-  it.skip('should have a name', function() {
+  it('should have a name', function() {
     // instantiate a Medusa object with your name as an argument
     // instantiate another Medusa object with someone else's name as an argument
+    var megara = new Medusa('Megara');
+    var maya = new Medusa('Maya');
 
     // assert that the first medusa's name is your name
     // assert that the second medusa's name is the other name
+    assert.equal(megara.name, 'Megara');
+    assert.equal(maya.name, 'Maya');
 
     //NOTE: We will not ask you to write your own tests in a mod 1 assessment, but it is good practice to prepare for mod 2!
   });
 
-  it.skip('should start with no statues', function() {
+  it('should start with no statues', function() {
     var medusa = new Medusa('Amy');
 
     assert.deepEqual(medusa.statues, []);
