0a71e32099d90b91e25c6b1d8f2f75479ce08cfa
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-14 23:53:30 -0500

    Completed pirate.js & updated README.md

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -2,7 +2,26 @@ class Pirate {
   constructor(name, job, curse) {
     this.name = name;
     this.job = job || "scallywag";
-    this.cursed = curse || "false";
+    this.cursed = curse || false;
+    this.booty = 0;
+  }
+  robShip() {
+    if (this.booty < 500) {
+      this.booty += 100;
+      return "YAARRR!"
+    } else {
+      this.cursed = true;
+      return "ARG! I've been cursed!";
+    }
+  }
+  liftCurse() {
+    if (this.booty >= 300 && this.cursed) {
+      this.cursed = false;
+      this.booty -= 300;
+      return "Your curse has been lifted!";
+    } else {
+      return "You don't need to lift a curse!";
+    }
   }
 }
 
