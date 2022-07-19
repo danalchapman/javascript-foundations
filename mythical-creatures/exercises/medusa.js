d9ce026fcf5f8a7e98bdfb02dcf9d791bebd4679
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-18 20:52:26 -0500

    Finished medusa.js with intention of repeating

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -6,6 +6,18 @@ class Medusa {
     this.name = name;
     this.statues = [];
   }
+  gazeAtVictim(victim) {
+    var newStatue = new Statue(victim.name);
+    this.statues.push(newStatue);
+    if (this.statues.length > 3) {
+      var statue = this.statues[0];
+      this.statues.shift();
+      var freePerson = new Person(statue.name);
+      freePerson.mood = "relieved";
+      return freePerson
+    }
+  }
 }
+
 
 module.exports = Medusa;
