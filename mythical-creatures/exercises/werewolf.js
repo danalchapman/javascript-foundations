26fcfdcdb7276b24f393bd62a31b5c9b6618e29a
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-20 16:45:26 -0500

    Completed werewolf.js and updated README.md

--- mythical-creatures/exercises/werewolf.js
+++ mythical-creatures/exercises/werewolf.js
@@ -1,7 +1,32 @@
+var Victim = require('./victim');
+
 class Werewolf {
   constructor(werewolfName) {
     this.name = werewolfName;
     this.form = "human";
+    this.hungry = false;
+  }
+  completeTransformation() {
+    if (this.form === "human") {
+      this.form = "wolf";
+      this.hungry = true;
+      return "Aaa-Woooo!";
+    } else if (this.form === "wolf") {
+      this.form = "human";
+      this.hungry = false;
+      return "Where are I?"
+    }
+  }
+  eatVictim(victim) {
+    // can eat a victim if hungry
+    var newVictim = new Victim(victim.name);
+    if (this.hungry === true) {
+      victim.alive = false;
+      this.form = "human";
+      return `Yum, ${victim.name} was delicious.`
+    } else if (this.form === "human") {
+      return `No way am I eating ${victim.name}, I'd like a burger!`
+    }
   }
 }
 
