66a75f327526f9436360a8a1dc14c492d4ef6ff9
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-26 15:35:35 -0500

    Refactor README.md and start fairy.js

---
+++ mythical-creatures/exercises/fairy.js
@@ -0,0 +1,18 @@
+class Fairy {
+  constructor(fairName) {
+    this.name = fairName;
+    this.dust = 10;
+    this.clothes = {dresses: ['Iris']};
+  }
+  receiveBelief() {
+    this.dust += 1;
+  }
+  believe() {
+    this.dust += 10;
+  }
+  // makeDresses(flowers) {
+  //   this.clothes.dresses.push(flowers);
+  // }
+}
+
+module.exports = Fairy;
