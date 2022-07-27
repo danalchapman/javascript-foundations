8ea3a9c58230bcb2691d2bcbc796b62b794af542
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-27 09:45:22 -0500

    Complete dragon.js for the second time

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,23 +1,19 @@
 class Dragon {
-  constructor(name, rider) {
-    this.name = name;
-    this.rider = rider;
-    this.greet = function() {
-      return `Hi, ${rider}!`
-    };
+  constructor(dragName, dragRider) {
+    this.name = dragName;
+    this.rider = dragRider;
     this.hungry = true;
     this.count = 0;
-    this.eat = function() {
-      this.count += 1;
-      if (this.count === 3) {
-        this.hungry = false;
-      } else {
-        this.hungry = true
-      }
+  }
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat() {
+    this.count += 1;
+    if (this.count >= 3) {
+      this.hungry = false;
     }
   }
 }
-
-
 
 module.exports = Dragon;
