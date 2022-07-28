a74da6a24d92fd4fb3d59abc81ca56e7ae1a299d
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-28 09:44:54 -0500

    Start medusa.js for the FIFTH TIME

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,22 +1,12 @@
-var Person = require('./person');
-var Statue = require('./statue');
-
 class Medusa {
   constructor(medusaName) {
     this.name = medusaName;
     this.statues = [];
   }
-  gazeAtVictim(victim) {
-    var newStatue = new Statue(victim.name)
-    this.statues.push(newStatue);
-    if (this.statues.length > 3) {
-      var statue = this.statues[0];
-      this.statues.shift();
-      var freePerson = new Person(statue.name);
-      freePerson.mood = "relieved";
-      return freePerson
-    }
-  }
+  // gazeAtVictim(victim) {
+  //   var newStatue = new Statue(victim);
+  //   this.statues.push(newStatue);
+  // }
 }
 
 module.exports = Medusa;
