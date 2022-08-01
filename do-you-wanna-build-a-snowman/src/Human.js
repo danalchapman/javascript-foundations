834f70a0a5850e4b146188bd66621445bad7251c
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-08-01 09:55:21 -0500

    Work on Human.js

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,21 @@
 var Snowman = require('./Snowman');
 
 class Human {
-
+    constructor(humanName) {
+        this.name = humanName;
+        this.wantsToBuildASnowman = true;
+        this.materials = {
+            snowballs: 0,
+            coal: 0,
+            buttons: 0,
+            carrots: 0
+        };
+    }
+    gatherMaterials(keyName, amount) { // passing in 2 arguments
+        if (this.materials === keyName) {
+            this.materials[keyName] + amount
+        }
+    }
 }
 
 module.exports = Human;
--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,16 @@
 class Snowman {
-
+    constructor(snowInfo) {
+        this.carrots = snowInfo.carrots;
+        this.coal = snowInfo.coal;
+        this.buttons = snowInfo.buttons;
+        this.snowballs = snowInfo.snowballs;
+        this.magicHat = false;
+    }
+    canWearMagicHat() {
+        if (this.coal >=2 && this.buttons >= 5 && this.carrots >= 1 && this.snowballs >= 2) {
+            this.magicHat = true;
+        }
+    }
 }
 
 module.exports = Snowman;
